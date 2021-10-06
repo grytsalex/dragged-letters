165315fbb8235fd58fa4f760f699b47deb3f1b43
Aleksandr Hrytsenko <32422951+grytsalex@users.noreply.github.com>
2021-10-06 16:32:53 +0300

    add span for every letter

--- script.js
+++ script.js
@@ -5,7 +5,28 @@ applyBtn.addEventListener("click", () => {
     
     if(inputValue == "") return;
 
-    let textField = document.querySelector(".text-field");
-    let arrayOfLetters = inputValue.split(" ");
-    textField.innerText = inputValue;
+    let textContainer = document.querySelector(".text-container");
+    let arrayOfLetters = inputValue.split("");
+    console.log(arrayOfLetters)
+
+    textContainer.innerHTML = arrayOfLetters.map((value) => {
+       return `<span class="letter">${value}</span>`
+    })   
 });
+
+
+// let applyBtn = document.querySelector(".apply-btn");
+// let inputValue = document.querySelector(".text-container");
+// let input = document.querySelector(".input-field");
+
+
+
+// applyBtn.addEventListener("click", () => {
+//     let arrayOfLetters = input.value.split("");
+//     let arrayOfNewSpans = arrayOfLetters.map(value => {
+
+//         let newSpan = document.createElement('span')
+//         newSpan.innerHTML = value;
+//         inputValue.appendChild(newSpan)
+//     })
+// });
