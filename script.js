b698ab90967e37b9979721b0aae5fe332d4fad0b
Aleksandr Hrytsenko <alkatraz0704@gmail.com>
2021-10-22 15:15:13 +0300

    add clicked class

--- script.js
+++ script.js
@@ -1,48 +1,61 @@
 let applyBtn = document.querySelector(".apply-btn");
 let container = document.querySelector(".container");
 
-container.addEventListener('click', (e) => {
-    e.stopPropagation()
-})
+container.addEventListener("click", function (e) {
+  e.stopPropagation();
+});
 
-applyBtn.addEventListener("click", () => {
-    applyBtn.disabled = true;
+applyBtn.addEventListener("click", function () {
+  let inputValue = document.querySelector(".input-field").value;
+  if (inputValue === "") return;
+  applyBtn.disabled = true;
 
-    let inputValue = document.querySelector(".input-field").value;
+  let textContainer = document.querySelector(".text-container");
+  let arrayOfLetters = inputValue.split("");
 
-    if(inputValue === "") return;
+  arrayOfLetters.map(function (value, index) {
+    if (value === " ") return;
 
-    let textContainer = document.querySelector(".text-container");
-    let arrayOfLetters = inputValue.split("");
-    
-    arrayOfLetters.map((value) => {
-        if(value === " ") return;
-        
-       let letterSpan = document.createElement("span");
-       letterSpan.classList.add("letters-span");
+    let letterSpan = document.createElement("span");
+    letterSpan.setAttribute("id", `el${index}`);
+    letterSpan.classList.add("letters-span");
 
-        letterSpan?.addEventListener("click", function () {
+    letterSpan.innerHTML = value;
+    textContainer.appendChild(letterSpan);
 
-            const onMouseMove = (e) => {
-                letterSpan.style.position = 'absolute';
-                let left = ((((e.clientX/window.innerWidth) * 100) - 40) * window.innerWidth) / 100;
-                let top = ((((e.clientY/window.innerHeight) * 100) - 40) * window.innerHeight) / 100;
+    letterSpan?.addEventListener("click", function (e) {
+      getCoordinates(e.clientX, e.clientY);
 
-                console.log(left)
-                letterSpan.style.left = left + 'px';
-                letterSpan.style.top = top + 10 + 'px';
-            }
+      letterSpan.classList.add("clicked");
 
-            document.addEventListener('mousemove', onMouseMove)
-            document.addEventListener('click', (e) =>{
-                const classes = ['letters-span', 'apply-btn', 'input-field']
+      function onMouseMove(e) {
+        letterSpan.style.position = "absolute";
 
-                if(classes.some(element => e.target.classList.contains(element))) return;
+        let left =
+          (((e.clientX / window.innerWidth) * 100 - 40) * window.innerWidth) /
+          100;
+        let top =
+          (((e.clientY / window.innerHeight) * 100 - 40) * window.innerHeight) /
+          100;
 
-                document.removeEventListener('mousemove', onMouseMove)
-            })
-        })
-       letterSpan.innerHTML = value;
-       textContainer.appendChild(letterSpan);
-    })   
+        letterSpan.style.left = left + "px";
+        letterSpan.style.top = top + 10 + "px";
+      }
+
+      document.addEventListener("mousemove", onMouseMove);
+
+      document.addEventListener("click", function (e) {
+        document.querySelectorAll(".letters-span").forEach((elem) => {
+          elem.classList.remove("clicked");
+        });
+
+        const classes = ["apply-btn", "input-field"];
+
+        if (classes.some((element) => e.target.classList.contains(element)))
+          return;
+
+        document.removeEventListener("mousemove", onMouseMove);
+      });
+    });
+  });
 });
